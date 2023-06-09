5eb09e8873631edec3e0e369fe0b2a6edcf43b8e
AnnaTsygalnyuk <nika314@list.ru>
2023-06-09 10:35:23 +0200

    Зафиксировали

---
+++ square/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number*number;
+Console.WriteLine("Квадрат числа равен: " + result);
